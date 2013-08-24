4a82122d615757c7e6bf06a4028522705b8a3acf
Carl Schwope <schwope.carl@gmail.com>
2013-08-24 11:54:06 -0400

    adds full title to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  # returns a full title on a page by page basis
+  def full_title short_title
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if short_title.empty?
+      base_title
+    else
+      "#{base_title} | #{short_title}"
+    end
+  end
+
+
 end
