6af475a9dfcf08c6601f402739b2ca84ce73c789
Carl Schwope <schwope.carl@gmail.com>
2013-08-24 19:15:45 -0400

    rspec uses support/utilities.rb

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,9 +8,9 @@ describe "Static pages" do
 
     before { visit root_path }
 
-    it { should have_selector        'h1', :text => 'Sample App' }
-    it { should have_selector     'title', :text => "Ruby on Rails Tutorial Sample App" }
-    it { should_not have_selector 'title', :text => '| Home' }
+    it { should have_selector        'h1', text: 'Sample App' }
+    it { should have_selector     'title', text: full_title('') }
+    it { should_not have_selector 'title', text: '| Home' }
 
   end
 
@@ -18,8 +18,8 @@ describe "Static pages" do
 
     before { visit help_path }
 
-    it { should have_selector('h1', :text => 'Help') }
-    it { should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help") }
+    it { should have_selector 'h1'   , text: 'Help' }
+    it { should have_selector 'title', text: full_title('Help') }
 
   end
 
@@ -27,8 +27,8 @@ describe "Static pages" do
 
     before { visit about_path }
 
-    it { should have_selector('h1', :text => 'About Us') }
-    it { should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About Us") }
+    it { should have_selector 'h1'   , text: 'About Us' }
+    it { should have_selector 'title', text: full_title('About Us') }
 
   end
 
@@ -36,8 +36,8 @@ describe "Static pages" do
 
     before { visit contact_path }
 
-    it { should have_selector('h1', :text => 'Contact Us') }
-    it { should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Contact Us") }
+    it { should have_selector 'h1'   , text: 'Contact Us' }
+    it { should have_selector 'title', text: full_title('Contact Us') }
 
   end
 end
