3482a0adf258d9478fac1c0fa7e6cf7373edfa78
Carl Schwope <schwope.carl@gmail.com>
2013-08-25 14:16:54 -0400

    passes tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,15 +10,22 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password, :password_confirmation
+  has_secure_password
 
-  validates_length_of   :name, maximum: 50
   validates_presence_of :name
+  validates_length_of   :name, maximum: 50
 
   validates_presence_of   :email
   validates_format_of     :email, with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates_uniqueness_of :email, case_sensitive: false
 
   before_save { |user| user.email = email.downcase }
+
+  validates_presence_of :password
+  validates_length_of   :password,  minimum: 6
+
+  validates_presence_of :password_confirmation
+
 
 end
