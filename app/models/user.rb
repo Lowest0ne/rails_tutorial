1cf2b54fd58b74a6149b25d064328f11a459b444
Carl Schwope <schwope.carl@gmail.com>
2013-08-25 12:42:11 -0400

    adds various User tests - passing

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,11 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
 
-  validates :name, presence: true
+  validates_length_of   :name, maximum: 50
+  validates_presence_of :name
+
+  validates_presence_of   :email
+  validates_format_of     :email, with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates_uniqueness_of :email, case_sensitive: false
+
 end
