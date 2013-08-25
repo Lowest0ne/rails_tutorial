52f42e88c61f039bba6bd75aaa3b51ad8c4b2150
Carl Schwope <schwope.carl@gmail.com>
2013-08-25 13:31:12 -0400

    added before_save to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,6 @@ class User < ActiveRecord::Base
   validates_format_of     :email, with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates_uniqueness_of :email, case_sensitive: false
 
+  before_save { |user| user.email = email.downcase }
+
 end
