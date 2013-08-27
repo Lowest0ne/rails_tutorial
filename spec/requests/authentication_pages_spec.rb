e8f6ae8ca1fc7ae80b3c757864795283ba202274
Carl Schwope <schwope.carl@gmail.com>
2013-08-27 16:58:38 -0400

    remove sing up link if signed in

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -23,6 +23,7 @@ describe "Authentication" do
       describe "after visiting another page" do
         before { click_link "Home" }
         it { should_not have_selector('div.alert.alert-error') }
+        it { should have_link('Sign up now!', href: signup_path ) }
       end
 
     end
@@ -39,7 +40,15 @@ describe "Authentication" do
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
+
+      describe "the home page should not allow you to sign up" do
+        before { click_link "Home" }
+        it { should_not have_link('Sign up now!', href: signup_path ) }
+      end
+
     end
+
+
 
   end
 
